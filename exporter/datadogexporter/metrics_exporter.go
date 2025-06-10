d61fa62bd4dee4c3bf8ae5cbefab7bc30eec74ad
Josh Carp <jm.carp@gmail.com>
2025-06-10 18:32:02 -0400

    datadogexporter: expose metric series interval.
    Optionally configure metric interval via datadog exporter config.

--- exporter/datadogexporter/metrics_exporter.go
+++ exporter/datadogexporter/metrics_exporter.go
@@ -228,6 +228,12 @@ func (exp *metricsExporter) PushMetricsData(ctx context.Context, md pmetric.Metr
 		var ms []datadogV2.MetricSeries
 		ms, sl = consumer.(*metrics.Consumer).All(exp.getPushTime(), exp.params.BuildInfo, tags, metadata)
 		if len(ms) > 0 {
+			interval := exp.cfg.Metrics.ExporterConfig.Interval
+			if interval != 0 {
+				for i := range ms {
+					ms[i].SetInterval(interval)
+				}
+			}
 			exp.params.Logger.Debug("exporting native Datadog payload", zap.Any("metric", ms))
 			_, experr := exp.retrier.DoWithRetries(ctx, func(context.Context) error {
 				ctx = clientutil.GetRequestContext(ctx, string(exp.cfg.API.Key))
